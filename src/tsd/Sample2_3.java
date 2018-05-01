6c6a0902c0e8ba62ebe4607680733c1d9f90d323
kanauchi <kanauchi@tsd-201601-0009>
2018-05-01 15:05:58 +0900

    int aをcに変更

--- src/tsd/Sample2_3.java
+++ src/tsd/Sample2_3.java
@@ -3,15 +3,15 @@ package tsd;
 public class Sample2_3 {
 
 	public static void main(String[] args) {
-		int a;
-		a = 100;
+		int c;
+		c = 100;
 		int b = 300;
 		
-		System.out.println(a);
+		System.out.println(c);
 		System.out.println(b);
 		
-		a = 200;
-		System.out.println(a);
+		c = 200;
+		System.out.println(c);
 	}
 
 }
